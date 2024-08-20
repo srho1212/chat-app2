98e07238fedb4d4ee05d83f911602b7c2335fb67
srho1212 <si990009@yahoo.co.jp>
2024-08-21 01:41:53 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :autheticate_user!
+  before_action :confingure_permitted_parameters, if: : device_controller?
+
+ private
+ def confingure_permitted_parameters
+  device_parameter_sanitizer.permit(:sign_up,keys:[:name])
+ end
 end
